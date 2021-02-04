43bf463e7ce8c52f295aff5f79eadfd3969ddaf4
Kartik Kadwane <kartik.kadwane@noovosoft.com>
2021-02-04 23:07:01 +0530

    random commit

--- passwordgen.py
+++ passwordgen.py
@@ -14,6 +14,13 @@ def gen():
     s.extend(list(number))
     s.extend(list(symbol))
 
+    pass_len=int(input("Enter the Password Length:"))
+    s=[]
+    s.extend(list(capital))
+    s.extend(list(small))
+    s.extend(list(number))
+    s.extend(list(symbol))
+
     random.shuffle(s)
     passs=("".join(s[0:pass_len]))
     print(passs)
